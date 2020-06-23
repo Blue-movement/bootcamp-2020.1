9a0044214f145e1cb0ce7e4298039ca9042e856a
Daijon B <daijon.bereolacarso@bison.howard.edu>
2020-06-23 13:36:14 -0700

    a function to create a new node

--- week-4/C++/daijon-bereolacarson.cpp
+++ week-4/C++/daijon-bereolacarson.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 
 using namespace std;
-
+//Linked List Class
 class Node{
     public:
     int data;
     Node* next;
 };
 
+//Create a new node
+Node* newNode(int data){
+    Node* node = new Node();
+    node->data = data;
+    node->next = NULL;
+    return node;
+}
 int main(){
     cout << "Hello world!";
     return 0;
